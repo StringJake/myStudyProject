bed6e4323cc3a02d892e35ec390821c056030457
jake <1045814478@qq.com>
2020-08-23 22:54:15 +0800

    yml配置文件获取

--- SpringBoot01_yml/src/test/java/com/example/demo/DemoApplicationTests.java
+++ SpringBoot01_yml/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	Person person;
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
